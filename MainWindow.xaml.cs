10acef3e0f4f19993d0a5c360e34c2e3ecfb689d
Kamil Haraf <kamilharaf7@gmail.com>
2022-04-27 18:27:43 +0200

    Cleanup

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,226 +8,84 @@ namespace KinectWPF
 {
     public partial class MainWindow : Window
     {
-        KinectSensor sensor = KinectSensor.KinectSensors[0];
+        KinectSensor kinect = KinectSensor.KinectSensors[0];
 
-        private const double BodyCenterThickness = 10;
-        private const double ClipBoundsThickness = 10;
-        private readonly Brush centerPointBrush = Brushes.Blue;
-        private readonly Brush trackedJointBrush = new SolidColorBrush(Color.FromArgb(255, 68, 192, 68));
-        private readonly Brush inferredJointBrush = Brushes.Yellow;
-        private readonly Pen trackedBonePen = new Pen(Brushes.Green, 6);
-        private readonly Pen inferredBonePen = new Pen(Brushes.Gray, 1);
+        private readonly Pen skeletonPen = new Pen(Brushes.Black, 6);
         private DrawingImage imageSource;
-        private const double JointThickness = 3;
         private DrawingGroup drawingGroup;
-        private const float RenderWidth = 640.0f;
-        private const float RenderHeight = 480.0f;
 
         public MainWindow()
         {
             InitializeComponent();
-
-            Loaded += MainWindow_Loaded;
-            Unloaded += MainWindow_Unloaded;
+            KinectStart();
         }
 
-        void MainWindow_Unloaded(object sender, RoutedEventArgs e)
+
+        void KinectStart()
         {
-            sensor.Stop();
-        }
+            drawingGroup = new DrawingGroup();
+            imageSource = new DrawingImage(drawingGroup);
+            kinectImage.Source = imageSource;
 
-        void MainWindow_Loaded(object sender, RoutedEventArgs e)
-        {
-            this.drawingGroup = new DrawingGroup();
-            this.imageSource = new DrawingImage(this.drawingGroup);
-
-            Image.Source = imageSource;
-
-            try
+            if (kinect.Status == KinectStatus.Connected)
             {
-                if (sensor.Status == KinectStatus.Connected)
-                {
-                    sensor.Start();
-                    sensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Default;
-                    sensor.SkeletonFrameReady += sensor_SkeletonFrameReady;
-                    Message.Text = "Kinect Ready";
-                    Message.Background = new SolidColorBrush(Colors.Green);
-                    Message.Foreground = new SolidColorBrush(Colors.White);
-
-                    this.sensor.SkeletonStream.Enable();
-                }
-
-                else if (sensor.Status == KinectStatus.Disconnected)
-                {
-                    Message.Text = "Kinect Sensor is not Connected";
-                    Message.Background = new SolidColorBrush(Colors.Orange);
-                    Message.Foreground = new SolidColorBrush(Colors.Black);
-                }
-
-                else if (sensor.Status == KinectStatus.NotPowered)
-                {
-                    Message.Text = "Kinect Sensor is not Powered";
-                    Message.Background = new SolidColorBrush(Colors.Red);
-                    Message.Foreground = new SolidColorBrush(Colors.Black);
-                }
-            }
-
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
+                kinect.Start();
+                kinect.SkeletonFrameReady += SkeletonFrameReady;
+                kinect.SkeletonStream.Enable();
             }
         }
 
-        void sensor_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        void SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
         {
-            Skeleton[] skeletons = new Skeleton[1];
+            Skeleton[] skeletons;
 
             using (SkeletonFrame skeletonframe = e.OpenSkeletonFrame())
             {
-
                 if (skeletonframe != null)
                 {
                     skeletons = new Skeleton[skeletonframe.SkeletonArrayLength];
                     skeletonframe.CopySkeletonDataTo(skeletons);
+                    DrawSkeletons(skeletons);
+                }
+            }
+        }
 
-                    if (sensor.SkeletonStream.TrackingMode == SkeletonTrackingMode.Default)
+        private void DrawSkeletons(Skeleton[] skeletons)
+        {
+            using (DrawingContext dc = drawingGroup.Open())
+            {
+                foreach (Skeleton skel in skeletons)
+                {
+                    if (skel.TrackingState == SkeletonTrackingState.Tracked)
                     {
-                        DrawStandingSkeletons(skeletons);
-                    }
-
-                    else if (sensor.SkeletonStream.TrackingMode == SkeletonTrackingMode.Seated)
-                    {
-                        DrawSeatedSkeletons(skeletons);
+                        DrawSkeleton(skel, dc);
                     }
                 }
             }
         }
 
-        private void DrawStandingSkeletons(Skeleton[] skeletons)
+        private void DrawSkeleton(Skeleton skeleton, DrawingContext drawingContext)
         {
-            using (DrawingContext dc = this.drawingGroup.Open())
-            {
-                dc.DrawRectangle(Brushes.Black, null, new Rect(0.0, 0.0, RenderWidth, RenderHeight));
-
-                if (skeletons.Length != 0)
-                {
-
-                    foreach (Skeleton skel in skeletons)
-                    {
-                        RenderClippedEdges(skel, dc);
-
-                        if (skel.TrackingState == SkeletonTrackingState.Tracked)
-                        {
-                            this.DrawBonesAndJoints(skel, dc);
-                        }
-
-                        else if (skel.TrackingState == SkeletonTrackingState.PositionOnly)
-                        {
-                            dc.DrawEllipse(this.centerPointBrush, null, this.SkeletonPointToScreen(skel.Position), BodyCenterThickness, BodyCenterThickness);
-                        }
-                    }
-                }
-
-                this.drawingGroup.ClipGeometry = new RectangleGeometry(new Rect(0.0, 0.0, RenderWidth, RenderHeight));
-            }
-        }
-
-        private void DrawSeatedSkeletons(Skeleton[] skeletons)
-        {
-            using (DrawingContext dc = this.drawingGroup.Open())
-            {
-
-                dc.DrawRectangle(Brushes.Black, null, new Rect(0.0, 0.0, RenderWidth, RenderHeight));
-
-                if (skeletons.Length != 0)
-                {
-                    foreach (Skeleton skel in skeletons)
-                    {
-                        RenderClippedEdges(skel, dc);
-
-                        if (skel.TrackingState == SkeletonTrackingState.Tracked)
-                        {
-                            this.DrawBonesAndJoints(skel, dc);
-                        }
-
-                        else if (skel.TrackingState == SkeletonTrackingState.PositionOnly)
-                        {
-                            dc.DrawEllipse(this.centerPointBrush, null, this.SkeletonPointToScreen(skel.Position), BodyCenterThickness, BodyCenterThickness);
-                        }
-                    }
-                }
-
-                this.drawingGroup.ClipGeometry = new RectangleGeometry(new Rect(0.0, 0.0, RenderWidth, RenderHeight));
-
-            }
-        }
-
-        private static void RenderClippedEdges(Skeleton skeleton, DrawingContext drawingContext)
-        {
-
-            if (skeleton.ClippedEdges.HasFlag(FrameEdges.Bottom))
-            {
-                drawingContext.DrawRectangle(Brushes.Red, null, new Rect(0, RenderHeight - ClipBoundsThickness, RenderWidth, ClipBoundsThickness));
-            }
-
-            if (skeleton.ClippedEdges.HasFlag(FrameEdges.Top))
-            {
-                drawingContext.DrawRectangle(Brushes.Red, null, new Rect(0, 0, RenderWidth, ClipBoundsThickness));
-            }
-
-            if (skeleton.ClippedEdges.HasFlag(FrameEdges.Left))
-            {
-                drawingContext.DrawRectangle(Brushes.Red, null, new Rect(0, 0, ClipBoundsThickness, RenderHeight));
-            }
-
-            if (skeleton.ClippedEdges.HasFlag(FrameEdges.Right))
-            {
-                drawingContext.DrawRectangle(Brushes.Red, null, new Rect(RenderWidth - ClipBoundsThickness, 0, ClipBoundsThickness, RenderHeight));
-            }
-        }
-
-        private void DrawBonesAndJoints(Skeleton skeleton, DrawingContext drawingContext)
-        {
-            this.DrawBone(skeleton, drawingContext, JointType.Head, JointType.ShoulderCenter);
-            this.DrawBone(skeleton, drawingContext, JointType.ShoulderCenter, JointType.ShoulderLeft);
-            this.DrawBone(skeleton, drawingContext, JointType.ShoulderCenter, JointType.ShoulderRight);
-            this.DrawBone(skeleton, drawingContext, JointType.ShoulderCenter, JointType.Spine);
-            this.DrawBone(skeleton, drawingContext, JointType.Spine, JointType.HipCenter);
-            this.DrawBone(skeleton, drawingContext, JointType.HipCenter, JointType.HipLeft);
-            this.DrawBone(skeleton, drawingContext, JointType.HipCenter, JointType.HipRight);
-            this.DrawBone(skeleton, drawingContext, JointType.ShoulderLeft, JointType.ElbowLeft);
-            this.DrawBone(skeleton, drawingContext, JointType.ElbowLeft, JointType.WristLeft);
-            this.DrawBone(skeleton, drawingContext, JointType.WristLeft, JointType.HandLeft);
-            this.DrawBone(skeleton, drawingContext, JointType.ShoulderRight, JointType.ElbowRight);
-            this.DrawBone(skeleton, drawingContext, JointType.ElbowRight, JointType.WristRight);
-            this.DrawBone(skeleton, drawingContext, JointType.WristRight, JointType.HandRight);
-            this.DrawBone(skeleton, drawingContext, JointType.HipLeft, JointType.KneeLeft);
-            this.DrawBone(skeleton, drawingContext, JointType.KneeLeft, JointType.AnkleLeft);
-            this.DrawBone(skeleton, drawingContext, JointType.AnkleLeft, JointType.FootLeft);
-            this.DrawBone(skeleton, drawingContext, JointType.HipRight, JointType.KneeRight);
-            this.DrawBone(skeleton, drawingContext, JointType.KneeRight, JointType.AnkleRight);
-            this.DrawBone(skeleton, drawingContext, JointType.AnkleRight, JointType.FootRight);
-
-
-            foreach (Joint joint in skeleton.Joints)
-            {
-                Brush drawBrush = null;
-
-                if (joint.TrackingState == JointTrackingState.Tracked)
-                {
-                    drawBrush = this.trackedJointBrush;
-                }
-
-                else if (joint.TrackingState == JointTrackingState.Inferred)
-                {
-                    drawBrush = this.inferredJointBrush;
-                }
-
-                if (drawBrush != null)
-                {
-                    drawingContext.DrawEllipse(drawBrush, null, this.SkeletonPointToScreen(joint.Position), JointThickness, JointThickness);
-                }
-            }
+            DrawBone(skeleton, drawingContext, JointType.Head, JointType.ShoulderCenter);
+            coordinatesLabel.Content = skeleton.Joints[JointType.Head].Position.X + ", " + skeleton.Joints[JointType.Head].Position.Y;
+            DrawBone(skeleton, drawingContext, JointType.ShoulderCenter, JointType.ShoulderLeft);
+            DrawBone(skeleton, drawingContext, JointType.ShoulderCenter, JointType.ShoulderRight);
+            DrawBone(skeleton, drawingContext, JointType.ShoulderCenter, JointType.Spine);
+            DrawBone(skeleton, drawingContext, JointType.Spine, JointType.HipCenter);
+            DrawBone(skeleton, drawingContext, JointType.HipCenter, JointType.HipLeft);
+            DrawBone(skeleton, drawingContext, JointType.HipCenter, JointType.HipRight);
+            DrawBone(skeleton, drawingContext, JointType.ShoulderLeft, JointType.ElbowLeft);
+            DrawBone(skeleton, drawingContext, JointType.ElbowLeft, JointType.WristLeft);
+            DrawBone(skeleton, drawingContext, JointType.WristLeft, JointType.HandLeft);
+            DrawBone(skeleton, drawingContext, JointType.ShoulderRight, JointType.ElbowRight);
+            DrawBone(skeleton, drawingContext, JointType.ElbowRight, JointType.WristRight);
+            DrawBone(skeleton, drawingContext, JointType.WristRight, JointType.HandRight);
+            DrawBone(skeleton, drawingContext, JointType.HipLeft, JointType.KneeLeft);
+            DrawBone(skeleton, drawingContext, JointType.KneeLeft, JointType.AnkleLeft);
+            DrawBone(skeleton, drawingContext, JointType.AnkleLeft, JointType.FootLeft);
+            DrawBone(skeleton, drawingContext, JointType.HipRight, JointType.KneeRight);
+            DrawBone(skeleton, drawingContext, JointType.KneeRight, JointType.AnkleRight);
+            DrawBone(skeleton, drawingContext, JointType.AnkleRight, JointType.FootRight);
         }
 
         private void DrawBone(Skeleton skeleton, DrawingContext drawingContext, JointType jointType0, JointType jointType1)
@@ -245,19 +103,12 @@ namespace KinectWPF
                 return;
             }
 
-            Pen drawPen = this.inferredBonePen;
-
-            if (joint0.TrackingState == JointTrackingState.Tracked && joint1.TrackingState == JointTrackingState.Tracked)
-            {
-                drawPen = this.trackedBonePen;
-            }
-
-            drawingContext.DrawLine(drawPen, this.SkeletonPointToScreen(joint0.Position), this.SkeletonPointToScreen(joint1.Position));
+            drawingContext.DrawLine(skeletonPen, SkeletonPointToScreen(joint0.Position), SkeletonPointToScreen(joint1.Position));
         }
 
         private Point SkeletonPointToScreen(SkeletonPoint skelpoint)
         {
-            DepthImagePoint depthPoint = this.sensor.CoordinateMapper.MapSkeletonPointToDepthPoint(skelpoint, DepthImageFormat.Resolution640x480Fps30);
+            DepthImagePoint depthPoint = kinect.CoordinateMapper.MapSkeletonPointToDepthPoint(skelpoint, DepthImageFormat.Resolution640x480Fps30);
 
             return new Point(depthPoint.X, depthPoint.Y);
         }
