3e3b29438cc7015d34f9345fcf4c594e99a5227e
KiobaDev <jakub.sarecki.developer@gmail.com>
2022-05-16 21:24:27 +0200

    Adding a random generation of background points and a mode for debugging

---
+++ Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace KinectWPF
+{
+    public static class Constants
+    {
+        public const int CIRCLE_MIN_SIZE = 60;
+        public const int CIRCLE_MAX_SIZE = 90;
+
+        public const int CANVA_WIDTH = 1280;
+        public const int CANVA_HEIGHT = 820;
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -4,16 +4,18 @@ using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace KinectWPF
 {
     public partial class MainWindow : Window
     {
-        KinectSensor kinect = KinectSensor.KinectSensors[0];
+        KinectSensor kinect;
 
         private readonly Pen skeletonPen = new Pen(Brushes.White, 6);
         private DrawingImage imageSource;
         private DrawingGroup drawingGroup;
+        private int circlesCount;
 
         public MainWindow()
         {
@@ -24,17 +26,58 @@ namespace KinectWPF
 
         void KinectStart()
         {
-            drawingGroup = new DrawingGroup();
-            imageSource = new DrawingImage(drawingGroup);
-            kinectImage.Source = imageSource;
-
-            if (kinect.Status == KinectStatus.Connected)
+            try
             {
-                kinect.Start();
-                kinect.SkeletonFrameReady += SkeletonFrameReady;
-                kinect.SkeletonStream.Enable();
+                kinect = KinectSensor.KinectSensors[0];
+
+                drawingGroup = new DrawingGroup();
+                imageSource = new DrawingImage(drawingGroup);
+                kinectImage.Source = imageSource;
+
+                if (kinect.Status == KinectStatus.Connected)
+                {
+                    kinect.Start();
+                    kinect.SkeletonFrameReady += SkeletonFrameReady;
+                    kinect.SkeletonStream.Enable();
+                }
+            }
+            catch (Exception ex)
+            {
+                //DEBUG MODE
+                Debug();
             }
         }
+
+        #region DEBUG
+        private void Debug()
+        {
+            GenerateRandomCirclePoint();
+        }
+
+
+        private void GenerateRandomCirclePoint()
+        {
+            var rnd = new Random();
+            var circle = new Ellipse();
+
+            if (circlesCount <= 0)
+                canvas.Children.Clear();
+
+            int circleSize = rnd.Next(Constants.CIRCLE_MIN_SIZE, Constants.CIRCLE_MAX_SIZE);
+
+            circle.Fill = Brushes.Black;
+            circle.Width = circleSize;
+            circle.Height = circle.Width;
+
+            canvas.Children.Add(circle);
+
+            Canvas.SetLeft(circle, rnd.Next(100, Constants.CANVA_WIDTH));
+            Canvas.SetTop(circle, rnd.Next(100, Constants.CANVA_HEIGHT));
+
+            circlesCount++;
+        }
+
+        #endregion DEBUG
 
         void SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
         {
