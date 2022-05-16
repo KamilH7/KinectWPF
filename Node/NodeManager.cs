4fef8331541a8f8ed81fd3ce79ca584423b9b97f
Adam Grajdek <avi19990@gmail.com>
2022-05-16 15:06:49 +0200

    Added NodeManager class

---
+++ Node/NodeManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinectWPF.Node
+{
+    class NodeManager
+    {
+        private float width, height;
+        private List<Node> nodes;
+    }
+}
