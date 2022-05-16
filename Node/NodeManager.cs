f9a7bc7dbc19df9058d85acf0171f5bbabe759a8
Adam Grajdek <avi19990@gmail.com>
2022-05-16 15:16:06 +0200

    Added GenerateNodes function

--- Node/NodeManager.cs
+++ Node/NodeManager.cs
@@ -23,5 +23,16 @@ namespace KinectWPF.Node
         {
             nodes.Remove(removedNode);
         }
+
+        public void GenerateNodes(int nodeCount, Node.NodeType nodeType)
+        {
+            Random rnd = new Random();
+
+            for (int i = 0; i < nodeCount)
+            {
+                Node newNode = new Node((float)rnd.NextDouble() * width, (float)rnd.NextDouble() * height, nodeType);
+                AddNode(newNode);
+            }
+        }
     }
 }
