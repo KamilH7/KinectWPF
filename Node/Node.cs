1d326f8c94873652d3402e6cdf30671069808fcf
Adam Grajdek <avi19990@gmail.com>
2022-05-16 14:53:09 +0200

    Simple Node class

---
+++ Node/Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinectWPF.Node
+{
+    class Node
+    {
+        public enum NodeType { Click };
+
+        public float x, y;
+        public NodeType nodeType;
+    }
+}
