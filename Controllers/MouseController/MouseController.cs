f3710c03e9b63f59db51374927499175662d1c2c
Kamil Haraf <kamilharaf7@gmail.com>
2022-06-04 15:43:40 +0200

    Added mouse-based input system

---
+++ Controllers/MouseController/MouseController.cs
@@ -0,0 +1,33 @@
+﻿using KinectWPF.Controllers.KinectController;
+using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace KinectWPF.Controllers.MouseController
+{
+    class MouseController : IInputController
+    {
+        private float acceptanceRadius = 5f;
+        private MainWindow window;
+
+        public void Initialize(MainWindow window)
+        {
+            this.window = window;
+        }
+
+        public bool IsHoveringOver(Point point)
+        {
+            Point mousePosition = GetMousePositionOnScreen();
+
+            double sqrDistance = Math.Pow(point.X - mousePosition.X, 2) - Math.Pow(point.Y - mousePosition.Y, 2);
+            double distance = Math.Sqrt(sqrDistance);
+
+            return distance <= acceptanceRadius ? true : false;
+        }
+
+        private Point GetMousePositionOnScreen()
+        {
+            return window.PointToScreen(Mouse.GetPosition(window));
+        }
+    }
+}
