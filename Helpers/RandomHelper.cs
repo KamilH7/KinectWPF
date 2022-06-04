c34b8bb4e573f5adeadac8a72b3eb967d31a6548
KiobaDev <jakub.sarecki.developer@gmail.com>
2022-06-04 18:16:47 +0200

    Adding a photo generation for a node

---
+++ Helpers/RandomHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace KinectWPF.Helpers
+{
+    public static class RandomHelper
+    {
+        public static int GenerateRandomFruitType()
+        {
+            Random random = new Random();
+
+            return random.Next(0, 3);
+        }
+
+        public static int GenerateRandomLeftPosition()
+        {
+            Random random = new Random();
+
+            return random.Next(100, 900);
+        }
+    }
+}
--- Node/Node.cs
+++ Node/Node.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using KinectWPF.Enums;
+using KinectWPF.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 
 namespace KinectWPF.Node
 {
@@ -13,6 +17,7 @@ namespace KinectWPF.Node
         public float x, y;
         public float radius;
         public NodeType nodeType;
+        public Rectangle sprite;
 
         public Node(float x, float y, float radius, NodeType nodeType)
         {
@@ -20,12 +25,42 @@ namespace KinectWPF.Node
             this.y = y;
             this.radius = radius;
 
+            sprite = GetImage();
+
             this.nodeType = nodeType;
         }
 
         public Node()
         {
 
+        }
+
+        internal Rectangle GetImage()
+        {
+            ImageBrush fruitBrush = new ImageBrush();
+            int randomNumber = RandomHelper.GenerateRandomFruitType();
+
+            fruitBrush.ImageSource = new BitmapImage(GetFruitsInformations().FirstOrDefault(f => f.Key == randomNumber).Value);
+
+            var fruit = new Rectangle()
+            {
+                Tag = "Fruit",
+                Height = 110,
+                Width = 110,
+                Fill = fruitBrush
+            };
+
+            return fruit;
+        }
+
+        internal Dictionary<int, Uri> GetFruitsInformations()
+        {
+            return new Dictionary<int, Uri>()
+            {
+                {  (int)FruitTypeEnum.PAPAYA, new Uri(string.Concat(@"pack://application:,,,/",Assembly.GetExecutingAssembly().GetName().Name,@";component/",@"Images/papaya.png"), UriKind.Absolute) },
+                {  (int)FruitTypeEnum.BANANA, new Uri(string.Concat(@"pack://application:,,,/",Assembly.GetExecutingAssembly().GetName().Name,@";component/",@"Images/banana.png"), UriKind.Absolute) },
+                {  (int)FruitTypeEnum.PEACH, new Uri(string.Concat(@"pack://application:,,,/",Assembly.GetExecutingAssembly().GetName().Name,@";component/",@"Images/peach.png"), UriKind.Absolute) }
+            };
         }
     }
 }
